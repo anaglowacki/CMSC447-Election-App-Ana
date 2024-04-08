5f9fe86b926106fbb3c559fabc4ec81e628a3440
Chloe <Chloe@MSI>
2024-04-08 11:25:08 -0400

    Added or modified various model definitions for election queries Added folder for data access classes

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -1,3 +1,4 @@
+using ElectionAppLibrary.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -12,8 +13,26 @@ namespace BlazorUI
             // Add services to the container.
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
+            builder.Services.AddSingleton<LoginService>();
+			builder.Services.AddHttpClient("RepAddress", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri("https://www.googleapis.com/civicinfo/v2/representatives?key=AIzaSyDRPOb2Wy4TIGZ2HcSuXLxxuIoNytPGIzE&address=");
+            });
+			builder.Services.AddHttpClient("RepDivision", httpClient =>
+			{
+				httpClient.BaseAddress = new Uri("https://civicinfo.googleapis.com/civicinfo/v2/representatives/ocd-division%2Fcountry%3Aus%2Fstate%3Amd?key=AIzaSyDRPOb2Wy4TIGZ2HcSuXLxxuIoNytPGIzE");
+			});
+			builder.Services.AddHttpClient("Elections", httpClient =>
+			{
+				httpClient.BaseAddress = new Uri("https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyDRPOb2Wy4TIGZ2HcSuXLxxuIoNytPGIzE");
+			});
+			builder.Services.AddHttpClient("VoterInfo", httpClient =>
+			{
+				httpClient.BaseAddress = new Uri("https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyDRPOb2Wy4TIGZ2HcSuXLxxuIoNytPGIzE&address=");
+			});
 
-            var app = builder.Build();
+
+			var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
---
+++ ElectionAppLibrary/Models/ElectionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionAppLibrary.Models;
+//uses electionQuery
+public class ElectionModel
+{
+	public Election[] elections { get; set; }
+	public string kind { get; set; }
+}
+
+public class Election
+{
+	public string id { get; set; }
+	public string name { get; set; }
+	public string electionDay { get; set; }
+	public string ocdDivisionId { get; set; }
+}
