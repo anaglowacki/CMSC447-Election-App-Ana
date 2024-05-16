703f6c1c78610741c599b735790edd125236d221
Ana Glowacki <54968891+anaglowacki@users.noreply.github.com>
2024-05-16 01:56:25 -0400

    EL-16 added logic to search for congressional districts in API and display candidates from DB according to district. Added functionality to associate candidates with user account if any.

---
+++ ElectionAppLibrary/Models/CandidateModelDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionAppLibrary.Models
+{
+	public class CandidateModelDB
+	{
+		public int Id { get; }
+		public string OfficeName { get; set; }
+		public string DistrictNameAndNumber { get; set; }
+		public string CandidateLastName { get; set; }
+		public string CandidateFirstName { get; set; }
+		public string PoliticalParty { get; set; }
+		public string CandidateStatus { get; set; }
+		public string? PublicPhone { get; set; }
+		public string? Email { get; set; }
+		public string? Website { get; set; }
+		public string? Facebook { get; set; }
+		public string? Twitter { get; set; }
+		public string? Other { get; set; }
+	}
+}
